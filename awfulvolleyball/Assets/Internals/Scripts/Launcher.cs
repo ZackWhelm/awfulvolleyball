924e2037afabb53eb35a7ddbd5316020ce06765b
Zack Whelm <whelmgamedev@gmail.com>
2025-04-13 14:24:34 -0700

    TestLauncher

---
+++ awfulvolleyball/Assets/Internals/Scripts/Launcher.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    [Header("Traits")]
+	public float launchForce = 1.75f;
+    public Vector3 launchDirection = Vector3.up;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!GameManager.Instance.Runner.IsServer) return;
+
+        if (other.TryGetComponent(out Ball ball))
+        {
+
+            ball.rb.AddForce(launchDirection.normalized * launchForce, ForceMode.Impulse);
+        }
+    }
+
+}
